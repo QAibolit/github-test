5c859128af6fd03648a1724524f53bb3b628d66a
Dina <sd@myexe.net>
2025-08-15 12:40:50 +0300

    Add new tests

---
+++ src/test/java/github/GithubEnterpriseTest.java
@@ -0,0 +1,38 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.allOf;
+import static com.codeborne.selenide.Condition.enabled;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withTagAndText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GithubEnterpriseTest {
+
+    @BeforeAll
+    public static void setUp() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    public void openEnterprisePlatformPageTest() {
+        open("");
+        $("#hero-section-brand-heading").shouldHave(text("Build and ship software on a single, collaborative platform"));
+        $(withTagAndText("button", "Solutions")).shouldBe(visible).hover();
+        $(withTagAndText("button", "Solutions")).sibling(0).shouldBe(visible);
+        $("a[href$='/enterprise']").shouldBe(visible).click();
+
+        // Проверка загрузки страницы Enterprise
+        $("#hero-section-brand-heading").shouldBe(visible).shouldHave(text("The AI-powered developer platform"));
+        $(".enterprise-hero-subtitle").shouldBe(visible).shouldHave(text("To build, scale, and deliver secure software"));
+        $("a[href$='/enterprises/new']").shouldBe(allOf(visible, enabled));
+        $("a[href*='Contact+Sales']").shouldBe(allOf(visible, enabled));
+    }
+}
---
+++ src/test/java/herokuapp/DragAndDropTest.java
@@ -0,0 +1,49 @@
+package herokuapp;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.actions;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    public static void setUp() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    public void dragAndDropSquareUsingActionsTest() {
+        open("/drag_and_drop");
+
+        SelenideElement leftSquare = $("#column-a").shouldHave(text("A"));
+        SelenideElement rightSquare = $("#column-b").shouldHave(text("B"));
+
+        actions().clickAndHold(leftSquare).moveToElement(rightSquare).release().perform();
+
+        // Проверка, что прямоугольники поменялись местами
+        leftSquare.shouldHave(text("B"));
+        rightSquare.shouldHave(text("A"));
+    }
+
+    @Test
+    public void dragAndDropSquareUsingSelenideMethodTest() {
+        open("/drag_and_drop");
+        SelenideElement leftSquare = $("#column-a").shouldHave(text("A"));
+        SelenideElement rightSquare = $("#column-b").shouldHave(text("B"));
+
+        leftSquare.dragAndDrop(to(rightSquare));
+
+        // Проверка, что прямоугольники поменялись местами
+        leftSquare.shouldHave(text("B"));
+        rightSquare.shouldHave(text("A"));
+    }
+}
